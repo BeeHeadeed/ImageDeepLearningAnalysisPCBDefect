83a8ff575ab4551a42753869d05f6bcd83a2c689
juanjacoop <juanjapuentech@gmail.com>
2025-11-09 22:23:01 +0800

    changed: datapath in train.py to work in colab

--- code/train.py
+++ code/train.py
@@ -1,11 +1,15 @@
 from ultralytics import YOLO
+import os
 
 model = YOLO('yolov8n.pt')  # nano (smallest/fastest)
 
 
+
+data_path = os.path.join("data", "yolo_dataset", "dataset.yaml")
+
 # Train
 results = model.train(
-    data='data\yolo_dataset\dataset.yaml',
+    data= data_path, 
     epochs=50,           # Start with 50-100 for baseline
     imgsz=640,           # Image size
     batch=16,            # Adjust based on GPU memory
